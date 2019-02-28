ce27415d726ffa7332f7a1865b6044487a2453da
Irvin Sanchez <irvinsanchez_94@hotmail.com>
2019-02-28 15:00:19 -0600

    creacion de api jwt token

---
+++ api/login.php
@@ -0,0 +1,54 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Methods: POST");
+  header("Access-Control-Max-Age: 3600");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+  include_once '../config/database.php';
+  include_once 'objects/user.php';
+  include_once '../config/core.php';
+  include_once '../libs/BeforeValidException.php';
+  include_once '../libs/ExpiredException.php';
+  include_once '../libs/SignatureInvalidException.php';
+  include_once '../libs/JWT.php';
+  use \Firebase\JWT\JWT;
+  
+  $database = new Database();
+  $db = $database->getConnection();
+  $user = new User($db);
+
+  $data = json_decode(file_get_contents("php://input"));
+  $user->email = $data->params->email;
+  $email_exists = $user->emailExists();
+  // password_verify verifica si la contrasena del formulario es la misma nos pide dos parametros
+  // el primero es la contrasena del formulario
+  // el segundo es la contrasena encriptada del usuario
+  if ($email_exists && password_verify($data->params->password, $user->password)) {
+    $token = array(
+      "iss" => $iss,
+      "aud" => $aud,
+      "iat" => $iat,
+      "nbf" => $nbf,
+      "data" => array(
+          "id" => $user->id,
+          "firstname" => $user->firstname,
+          "lastname" => $user->lastname,
+          "email" => $user->email
+      )
+    );
+    http_response_code(200);
+    $jwt = JWT::encode($token, $key);
+    echo json_encode(
+      array(
+        "message" => "Successful login.",
+        "jwt" => $jwt
+      )
+    );
+  } else {
+    http_response_code(401);
+    echo json_encode(array("message" => "Login failed."));
+  }
+
+?>
+
---
+++ api/validate_token.php
@@ -0,0 +1,43 @@
+<?php
+
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Methods: POST");
+  header("Access-Control-Max-Age: 3600");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+  include_once '../config/core.php';
+  include_once '../libs/BeforeValidException.php';
+  include_once '../libs/ExpiredException.php';
+  include_once '../libs/SignatureInvalidException.php';
+  include_once '../libs/JWT.php';
+  use \Firebase\JWT\JWT;
+  //file_get_contents -> trae todo el contenido de la informacion de una ruta u objeto especifico en este caso del formulario que enviamos
+  // se envia un objeto JSON para ser consultado y luego transformado en un array y codificado a un JSON
+  $data = json_decode(file_get_contents("php://input")); 
+  // si data->jwt existe que me regrese el token y si no que lo envie vacio.
+  $jwt = isset($data->jwt) ? $data->jwt : "";
+  // si jwt no esta vacio
+  if ($jwt) {
+    try {
+      //decodificamos jwt con la JWT y la funcion decode
+      $decoded = JWT::decode($jwt, $key, array('HS256'));
+      http_response_code(200);
+      echo json_encode(array(
+          "message" => "Access granted.",
+          "data" => $decoded->data
+      ));
+    } catch (Exception $e) {
+      //mostramos error si ocurre al momemto de decodificarlo
+      http_response_code(401);
+      echo json_encode(array(
+          "message" => "Access denied.",
+          "error" => $e->getMessage()
+    ));
+    }
+  } else {
+    http_response_code(401);
+    echo json_encode(array("message" => "Access denied."));
+  }
+
+?>
