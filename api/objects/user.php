e486073f8e415c342c96ed673a58dca4978e9f99
IrvinS <isanchez@applaudostudios.com>
2019-08-03 19:10:42 -0600

    recuperacion

--- api/objects/user.php
+++ api/objects/user.php
@@ -133,17 +133,17 @@ class User
     return $stmt;
   }
 
-  function generateToken()//funcion para generar el token
-	{
-		$gen = md5(uniqid(mt_rand(), false));//mt_rand nos genera un valor dependiendo la hora y fecha del sistema, uniqid genera un identificador y luego lo pasa a md5
-		return $gen;
-	}
+  function generateToken() //funcion para generar el token
+  {
+    $gen = md5(uniqid(mt_rand(), false)); //mt_rand nos genera un valor dependiendo la hora y fecha del sistema, uniqid genera un identificador y luego lo pasa a md5
+    return $gen;
+  }
 
-  function generaTokenPass($user_id, $token)//esta funcion genera un token al solicitar cambio de password
-	{	
-		//se llama la funcion que genera los token pero luego hace un update 
-		
-    $query = "UPDATE usuarios SET PasswdTmp=?, password_request=1 WHERE IdUsuario = ?";//este query coloca el token generado en la BD ademas de hacer el update al campo password_request
+  function generaTokenPass($user_id, $token) //esta funcion genera un token al solicitar cambio de password
+  {
+    //se llama la funcion que genera los token pero luego hace un update 
+
+    $query = "UPDATE usuarios SET PasswdTmp=?, password_request=1 WHERE IdUsuario = ?"; //este query coloca el token generado en la BD ademas de hacer el update al campo password_request
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(1, $token);
     $stmt->bindParam(2, $user_id);
@@ -151,7 +151,89 @@ class User
       return true;
     }
     return false;
-	}
+  }
 
+  function enviarEmail($email, $nombre, $asunto, $cuerpo)
+  { //funcion de enviar email, recibe el email, el nombre, asunto y cuerpo
 
+    require_once '../../login/PHPMailer/PHPMailerAutoload.php'; //lebreria phpmailer se usa para el funcionamiento del envio de correo
+
+    $mail = new PHPMailer();
+    $mail->isSMTP(); // protocolo de transferencia de correo
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl'; //habilita la encriptacion SSL
+    $mail->Host = 'smtp.gmail.com'; // Servidor GMAIL
+    $mail->Port = 465; //puerto
+
+    $mail->Username = 'lapizzeriapassrecovery@gmail.com'; //correo emisor
+    $mail->Password = 'lapizzeria2019'; //contraseña del correo del emisor
+
+    $mail->setFrom('lapizzeriapassrecovery@gmail.com', 'La Pizzeria'); //se establece quien envia el correo
+    $mail->addAddress($email, $nombre); //email y nombre del receptor guardados en sus respectivas variables
+
+    $mail->Subject = $asunto; //se configura cual es el asunto del correo
+    $mail->Body    = $cuerpo; //se configura cual es el cuerpo del correo
+    $mail->IsHTML(true);
+
+    if ($mail->send())
+      return true;
+    else
+      return false;
+  }
+
+  function verificaTokenPass($user_id, $token)
+  { //esta funcion es para verificar que el ID y el token sean de un registro valido en la BD
+    //ademas verifica si el usuario a solicitado el cambio de password 
+
+    $query = "SELECT IdUsuario FROM usuarios WHERE IdUsuario = ? AND PasswdTmp = ? AND password_request = 1 LIMIT 1";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $user_id);
+    $stmt->bindParam(2, $token);
+    $stmt->execute();
+    $num = $stmt->rowCount();
+
+    if ($num > 0) {
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      $this->IdUsuario = $row['IdUsuario'];
+      if ($this->IdUsuario = $row['IdUsuario']) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  function validaPassword($var1, $var2) //funcion para validar el password y la confirmacion de pasword sean iguales
+  {
+    if (strcmp($var1, $var2) !== 0) { //strcmp hace una comparacion de datos string
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function hashPassword($password) //funcion para "encriptar contraseña"
+  {
+    $hash = password_hash($password, PASSWORD_DEFAULT); //password_hash($password, PASSWORD_DEFAULT) nos da el hash del password almacenado en la variable $password
+    return $hash;
+  }
+
+  function cambiaPassword($password, $user_id, $token)
+  { //esta funcion hace el update de la nueva password y de los otros campos
+
+    //la query actualiza la contraseña y cambia el campo password_request a 0 y el token_password lo limpia
+    $query = "UPDATE usuarios SET Passwd = ?, PasswdTmp='', password_request=0 WHERE IdUsuario = ? AND PasswdTmp = ?";
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $password);
+    $stmt->bindParam(2, $user_id);
+    $stmt->bindParam(3, $token);
+
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- api/user/recuperacion.php
+++ api/user/recuperacion.php
@@ -1,42 +1,52 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
-  header("Content-Type: application/json; charset=UTF-8");
-  header("Access-Control-Allow-Methods: *");
-  header("Access-Control-Max-Age: 3600");
-  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-  if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-    http_response_code(200);
-  } else if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    http_response_code(200);
-    echo "You dont have the correct method";
-  } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    http_response_code(200);
-    include_once '../../config/database.php';
-    include_once '../objects/user.php';
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+  http_response_code(200);
+} else if ($_SERVER['REQUEST_METHOD'] == "GET") {
+  http_response_code(200);
+  echo "You dont have the correct method";
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  http_response_code(200);
+  include_once '../../config/database.php';
+  include_once '../objects/user.php';
 
-    $database = new Database();
-    $db = $database->getConnection();
-    $user = new User($db);
-    $data = json_decode(file_get_contents("php://input"));
-    if (empty($data->Email)) {
-      echo json_encode(
-        array("message" => "EMPTY")
-      );
-    } else {
+  $database = new Database();
+  $db = $database->getConnection();
+  $user = new User($db);
+  $data = json_decode(file_get_contents("php://input"));
+  if (empty($data->Email)) {
+    echo json_encode(
+      array("message" => "EMPTY")
+    );
+  } else {
     $user->Email = $data->Email;
 
     if ($user->emailExists()) {
       http_response_code(200);
       $token = $user->generateToken();
       $user->generaTokenPass($user->IdUsuario, $token);
-      $url = 'http://localhost/login/cambia_pass.php?user_id='.$user->IdUsuario.'&token='.$token;
-      echo json_encode(
-        array(
-          "message" => "recuperacion",
-          "url" => $url,
-        )
-      );
+      $url = 'http://localhost/tesis/api-jws/login/cambia_pass.php?user_id=' . $user->IdUsuario . '&token=' . $token;
+      $asunto = 'Recuperar Password - Sistema de Usuarios';
+      $cuerpo = "Hola $user->Nombre: <br /><br />Se ha solicitado un reinicio de contrase&ntilde;a. <br/><br/>Para restaurar la contrase&ntilde;a, hacer clic en el siquiente link: <a href='$url'>Cambiar Password</a>";
+
+      if ($user->enviarEmail($data->Email, $user->Nombre, $asunto, $cuerpo)) {
+        echo json_encode(
+          array(
+            "message" => "recuperacion",
+            "url" => $url,
+          ),
+          JSON_UNESCAPED_SLASHES
+        );
+      } else {
+        echo json_encode(
+          array("ErrorSMTP" => "Problema al momento de enviar el EMAIL")
+        );
+      }
     } else {
       http_response_code(404);
       echo json_encode(
@@ -45,4 +55,5 @@
     }
   }
 } else {
-  http_response_code(404);}
+  http_response_code(404);
+}
